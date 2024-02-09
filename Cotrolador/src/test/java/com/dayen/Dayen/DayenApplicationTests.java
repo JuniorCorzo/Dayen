31e06744747fa943f1cb2120d23febb50c09f867
JuniorCorzo <josedanielmmf@gmail.com>
2024-02-09 12:04:33 -0500

    Inicilizado el proyecto de la api de Dayen

---
+++ Cotrolador/src/test/java/com/dayen/Dayen/DayenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayen.Dayen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
