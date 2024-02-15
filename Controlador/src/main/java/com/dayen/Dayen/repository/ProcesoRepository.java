472a1b90f043abc60c9a73f2742c5507a6bfa2ca
JuniorCorzo <josedanielmmf@gmail.com>
2024-02-15 14:16:36 -0500

    Se creo el repositorio de Procesos

---
+++ Controlador/src/main/java/com/dayen/Dayen/repository/ProcesoRepository.java
@@ -0,0 +1,7 @@
+package com.dayen.Dayen.repository;
+
+import com.dayen.Dayen.entity.Procesos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcesoRepository extends JpaRepository<Procesos, Integer> {
+}
