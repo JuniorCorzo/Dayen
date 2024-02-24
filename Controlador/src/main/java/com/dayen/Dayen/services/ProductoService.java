e8f02d36eaeb15ac811ac06b4252950e3b31f41f
JuniorCorzo <josedanielmmf@gmail.com>
2024-02-24 14:21:24 -0500

    Se creo el servicio de Productos

---
+++ Controlador/src/main/java/com/dayen/Dayen/services/ProductoService.java
@@ -0,0 +1,20 @@
+package com.dayen.Dayen.services;
+
+import com.dayen.Dayen.entity.Productos;
+import com.dayen.Dayen.repository.ProductoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService {
+	private final ProductoRepository productoRepository;
+
+	public ProductoService(ProductoRepository productoRepository) {
+		this.productoRepository = productoRepository;
+	}
+
+	public List<Productos> getAllProductos(){
+		return productoRepository.findAll();
+	}
+}
