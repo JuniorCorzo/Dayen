754041036790a807ad0d73e75b139ce9869d10b7
JuniorCorzo <josedanielmmf@gmail.com>
2024-02-24 17:57:06 -0500

    Se creo el controller de Personal

---
+++ Controlador/src/main/java/com/dayen/Dayen/controller/PersonalController.java
@@ -0,0 +1,26 @@
+package com.dayen.Dayen.controller;
+
+import com.dayen.Dayen.entity.Personal;
+import com.dayen.Dayen.services.PersonalService;
+import com.dayen.Dayen.services.ProcesoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/personal")
+public class PersonalController {
+	private final PersonalService personalService;
+
+	public PersonalController(PersonalService personalService) {
+		this.personalService = personalService;
+	}
+
+	@GetMapping("/{idUsuario}")
+	public List<Personal> getAllPersonalByUsuario(@PathVariable Integer idUsuario){
+		return this.personalService.getAllPersonalByUsuario(idUsuario);
+	}
+}
