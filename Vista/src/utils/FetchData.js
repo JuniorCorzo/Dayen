d55f790066266282b73adf51bf1c4dd17f82e286
JuniorCorzo <josedanielmmf@gmail.com>
2024-03-30 22:26:14 -0500

    REFACTOR: Se formateo el code con standard js

--- Vista/src/utils/FetchData.js
+++ Vista/src/utils/FetchData.js
@@ -1,33 +1,33 @@
-async function getData(url) {
+async function getData (url) {
   const response = await fetch(`${window.HOST_API}${url}`, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "content-type": "application/json",
-      Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
-    },
-  });
+      'content-type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('jwt')}`
+    }
+  })
 
-  if (!response.ok) throw Error(response.statusText);
+  if (!response.ok) throw Error(response.statusText)
 
-  const data = await response.json();
-  return data;
+  const data = await response.json()
+  return data
 }
 
-export function pushData(url, data, method = "POST") {
+export function pushData (url, data, method = 'POST') {
   return fetch(`${window.HOST_API}${url}`, {
     method: method.toUpperCase(),
     headers: {
-      "content-type": "application/json",
-      Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
+      'content-type': 'application/json',
+      Authorization: `Bearer ${sessionStorage.getItem('jwt')}`
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(data)
   })
     .then((response) => {
-      return response.json();
+      return response.json()
     })
     .catch((err) => {
-      throw Error(err);
-    });
+      throw Error(err)
+    })
 }
 
-export default getData;
+export default getData
