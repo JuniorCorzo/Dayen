b4458532a87eba833d5e9ce4fb070ee2fb3359b0
JuniorCorzo <josedanielmmf@gmail.com>
2024-04-02 23:38:40 -0500

    ADD: nombre and tituloImagen fields to LoteRequest and Lotes classes

--- Controlador/src/main/java/com/dayen/dayen/dao/request/LoteRequest.java
+++ Controlador/src/main/java/com/dayen/dayen/dao/request/LoteRequest.java
@@ -3,6 +3,8 @@ package com.dayen.dayen.dao.request;
 public record LoteRequest(
 		Integer idLote,
 		String idUsuario,
+		String nombre,
+		String tituloImagen,
 		String fase,
 		Integer hectareas
 ) {
