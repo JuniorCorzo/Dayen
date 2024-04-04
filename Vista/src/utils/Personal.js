54e7b28c725f8268eee85f4313ae7d1753de43a5
JuniorCorzo <josedanielmmf@gmail.com>
2024-04-04 16:00:20 -0500

    Add updatePersonalSession function call in postData function

--- Vista/src/utils/Personal.js
+++ Vista/src/utils/Personal.js
@@ -1,7 +1,9 @@
 import { pushData } from './FetchData'
+import { updatePersonalSession } from '../config/ManageSessionStorage'
 
 const postData = (data) => {
   pushData('/personal/create', data).then((data) => {
+    updatePersonalSession()
     alert(`Se registro el trabajador llamado ${data.nombre}`)
   })
 }
