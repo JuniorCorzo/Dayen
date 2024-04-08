595a3fed0a2a525e6e456f7ced679521b4cbc194
JuniorCorzo <josedanielmmf@gmail.com>
2024-04-07 21:22:11 -0500

    Add getAllProcesosOfPersonal method to PersonalController and PersonalService

--- Controlador/src/main/java/com/dayen/dayen/controller/PersonalController.java
+++ Controlador/src/main/java/com/dayen/dayen/controller/PersonalController.java
@@ -2,6 +2,7 @@ package com.dayen.dayen.controller;
 
 import com.dayen.dayen.dao.request.PersonalRequest;
 import com.dayen.dayen.entity.Personal;
+import com.dayen.dayen.entity.Procesos;
 import com.dayen.dayen.services.PersonalService;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,10 @@ public class PersonalController {
 		return this.personalService.getAllPersonalByUsuario(idUsuario);
 	}
 
+	@GetMapping("{idPersonal}/procesos")
+	public List<Procesos> getAllProcesosOfPersonal(@PathVariable Integer idPersonal){
+		return this.personalService.getAllProcesosOfPersonal(idPersonal);
+	}
 	@PostMapping("/create")
 	public Personal createPersonal(@RequestBody PersonalRequest personal){
 		return this.personalService.createPersonal(personal);
